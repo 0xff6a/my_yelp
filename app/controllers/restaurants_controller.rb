923157bb4c692b78c492f1f450e7042fffc5f996
Jeremy Fox <foxjerem@gmail.com>
2014-08-11 14:22:19 +0100

    Refactored restaurant controller"

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,20 +7,30 @@ class RestaurantsController < ApplicationController
 	def create
 		name, cuisine = params[:restaurant].values
 		new_restaurant = Restaurant.create(:name => name, :cuisine => cuisine)
-		if new_restaurant
-		 	flash[:notice] = 'Your restaurant has been added'
-		else 
-			flash[:errors] = new_restaurant.errors
-		end
-
-		@restaurants = Restaurant.all
-		render 'index'
+		new_restaurant ? _restaurant_create_success : _restaurant_create_error(new_restaurant.errors)
+		_display_index_view
 	end
 
 	def destroy
 		target_restaurant = Restaurant.find(params[:id])
 		target_restaurant.delete
+		_restaurant_delete_success
+		_display_index_view
+	end
+
+	def _restaurant_delete_success
 		flash[:notice] = 'The restaurant has been removed'
+	end
+
+	def _restaurant_create_success
+		flash[:notice] = 'Your restaurant has been added'
+	end
+
+	def _restaurant_create_error(errors)
+		flash[:errors] = errors
+	end
+
+	def _display_index_view
 		@restaurants = Restaurant.all
 		render 'index'
 	end
