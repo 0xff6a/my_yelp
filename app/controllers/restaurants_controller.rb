023f6ebae20ac9b7a77b39b86069fb9e914b311d
Jeremy Fox <foxjerem@gmail.com>
2014-08-11 15:34:57 +0100

    Refactored various post lecture

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,38 +1,40 @@
 class RestaurantsController < ApplicationController
 
 	def index
-		@restaurants = Restaurant.all
+		@restaurants = _restaurant_list
+	end
+
+	def new
+		@restaurant = Restaurant.new
 	end
 
 	def create
-		name, cuisine = params[:restaurant].values
-		new_restaurant = Restaurant.create(:name => name, :cuisine => cuisine)
+		new_restaurant = Restaurant.create(params[:restaurant].permit(:name, :cuisine))
 		new_restaurant ? _restaurant_create_success : _restaurant_create_error(new_restaurant.errors)
-		_display_index_view
 	end
 
 	def destroy
 		target_restaurant = Restaurant.find(params[:id])
 		target_restaurant.delete
 		_restaurant_delete_success
-		_display_index_view
 	end
 
 	def _restaurant_delete_success
 		flash[:notice] = 'The restaurant has been removed'
+		redirect_to '/restaurants'
 	end
 
 	def _restaurant_create_success
 		flash[:notice] = 'Your restaurant has been added'
+		redirect_to '/restaurants'
 	end
 
 	def _restaurant_create_error(errors)
 		flash[:errors] = errors
 	end
 
-	def _display_index_view
-		@restaurants = Restaurant.all
-		render 'index'
+	def _restaurant_list
+		Restaurant.all.reverse
 	end
 
 end
