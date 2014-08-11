348ba9e0f72d4bfde8a6625b07405da232b0bbd1
Jeremy Fox <foxjerem@gmail.com>
2014-08-11 13:04:54 +0100

    Added create functionality

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,13 @@ class RestaurantsController < ApplicationController
 		@restaurants = Restaurant.all
 	end
 
+	def create
+		name = params[:restaurant_name]
+		cuisine = params[:restaurant_cuisine]
+		Restaurant.create(:name => name, :cuisine => cuisine)
+		# flash[:notice] = 'Your restaurant has been added'
+		@restaurants = Restaurant.all
+		render 'index'
+	end
+
 end
--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -10,6 +10,17 @@ describe 'restaurants' do
 			expect(page).to have_link('Add a restaurant')
 		end
 
+		it 'should allow a user to add a restaurant' do
+			visit '/restaurants'
+			click_link 'Add a restaurant'
+			fill_in 'restaurant_name', :with => 'Red Dog Saloon'
+			fill_in 'restaurant_cuisine', :with => 'American'
+			click_on 'Add Restaurant'
+			expect(Restaurant.count).to eq(1)
+			expect(Restaurant.first.name).to eq('Red Dog Saloon')
+			expect(Restaurant.first.cuisine).to eq('American')
+		end	
+
 	end
 
 	context 'restaurants have been added' do
