609a8d731336f9e834c9434161f87b198768a898
Jeremy Fox <foxjerem@gmail.com>
2014-08-12 11:48:22 +0100

    Added user sign up and refactored

--- spec/features/user_management_feature_spec.rb
+++ spec/features/user_management_feature_spec.rb
@@ -5,14 +5,7 @@ describe 'User management feature:' do
 	context 'signing up' do
 
 		it 'a user can sign up to the site' do
-			visit restaurants_path
-			click_link 'Sign Up'
-			within('#new_user') do
-				fill_in 'user_email', with: 'test@test.com'
-				fill_in 'user_password', with: '12345678'
-				fill_in 'user_password_confirmation', with: '12345678'
-				click_on 'Sign up'
-			end
+			_sign_up_test_user
 			expect(page).to have_content('Welcome! You have signed up successfully')
 			expect(page).to have_content('test@test.com')
 		end
--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -1,37 +1,52 @@
-	def _create_sample_restaurant
-		Restaurant.create(name: 'Red Dog Saloon',
-												cuisine: 'Diner',				)
-	end
+def _create_sample_restaurant
+	Restaurant.create(name: 'Red Dog Saloon',
+											cuisine: 'Diner',				)
+end
 
-	def _review_sample_restaurant(rating, comment)
+def _review_sample_restaurant(rating, comment)
+	visit restaurants_path
+	click_link 'Review'
+	select(rating, :from => 'review_rating')
+	fill_in 'review_comment', with: comment
+	click_on 'Publish'
+end
+
+def _edit_restaurant(name, cuisine)
 		visit restaurants_path
-		click_link 'Review'
-		select(rating, :from => 'review_rating')
-		fill_in 'review_comment', with: comment
-		click_on 'Publish'
-	end
+		click_link 'Edit'			
+		fill_in 'restaurant_name', with: name if name
+		fill_in 'restaurant_cuisine', with: cuisine if cuisine
+		click_on 'Update Restaurant'
+end
 
-	def _edit_restaurant(name, cuisine)
-			visit restaurants_path
-			click_link 'Edit'			
-			fill_in 'restaurant_name', with: name if name
-			fill_in 'restaurant_cuisine', with: cuisine if cuisine
-			click_on 'Update Restaurant'
-	end
+def _add_restaurant(name, cuisine)
+	visit restaurants_path
+	click_link 'Add a restaurant'
+	fill_in 'restaurant_name', with: name
+	fill_in 'restaurant_cuisine', with: cuisine
+	click_on 'Add Restaurant'
+end
 
-	def _add_restaurant(name, cuisine)
-		visit restaurants_path
-		click_link 'Add a restaurant'
-		fill_in 'restaurant_name', with: name
-		fill_in 'restaurant_cuisine', with: cuisine
-		click_on 'Add Restaurant'
-	end
+def _delete_restaurant
+	visit restaurants_path
+	click_link 'Delete'
+end
 
-	def _delete_restaurant
-		visit restaurants_path
-		click_link 'Delete'
-	end
+def _restaurant
+	Restaurant.first
+end
 
-	def _restaurant
-		Restaurant.first
+def _sign_up_test_user
+	_sign_up_user('test@test.com', '12345678')
+end
+
+def _sign_up_user(email, password)
+	visit restaurants_path
+	click_link 'Sign Up'
+	within('#new_user') do
+		fill_in 'user_email', with: email
+		fill_in 'user_password', with: password
+		fill_in 'user_password_confirmation', with: password
+		click_on 'Sign up'
 	end
+end
