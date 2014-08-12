8dd535d47ed2d9076671093fd4562a33d338d685
Jeremy Fox <foxjerem@gmail.com>
2014-08-12 12:01:52 +0100

    Added sign out feature

--- spec/features/user_management_feature_spec.rb
+++ spec/features/user_management_feature_spec.rb
@@ -4,10 +4,34 @@ describe 'User management feature:' do
 
 	context 'signing up' do
 
-		it 'a user can sign up to the site' do
+		it 'a user can sign up to the site ' do
 			_sign_up_test_user
 			expect(page).to have_content('Welcome! You have signed up successfully')
 			expect(page).to have_content('test@test.com')
+		end
+
+	end
+
+	context 'signing in' do
+
+		before(:each) { _sign_up_test_user }
+
+		it 'a user can sign in to the site once signed up ' do
+	
+			expect(page).to have_content('Welcome! You have signed up successfully')
+			expect(page).to have_content('test@test.com')
+		end
+
+	end
+
+	context 'signing out' do
+
+		before(:each) { _sign_up_test_user }
+			
+		it 'a user can sign out once signed in' do
+			click_link 'Sign Out'
+			expect(page).to have_content('Signed out successfully')
+			expect(page).not_to have_content('test@test.com')
 		end
 
 	end
