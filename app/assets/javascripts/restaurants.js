744d3475b64111ff672c1379da65f7d8d6a96849
Jeremy Fox <foxjerem@gmail.com>
2014-08-14 17:38:48 +0100

    Added JS widget

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
 
-	$('.restaurant-input-form').dialog({autoOpen: false, 
-																			resizeable: false, 
-																			title: 'Add a new resturant'})
+	var popUp = $('<div></div>')
+
+	popUp.dialog({autoOpen: false, 
+								modal: true,
+								resizeable: false, 
+								title: 'Add a new restaurant',
+								minHeight: 250,
+								minWidth: 400
+	});
 
 	$('#new-restaurant-link').click(function(){
 		 event.preventDefault();
-	})
-})
+		 popUp.load('restaurants/new.html .restaurant-input-form');
+		 popUp.dialog('open');
+
+	});
+
+});
+
